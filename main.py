44c975ff789c62fe1240f5f74d220024bc895599
nikita123432 <nikita04Mar@gmail.com>
2024-09-19 12:31:50 +0300

    commit

---
+++ main.py
@@ -0,0 +1,32 @@
+
+
+from fastapi import FastAPI
+
+
+from contextlib import asynccontextmanager
+
+from datebase import create_tables, delete_tables
+from router import router as tasks_router
+
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    await delete_tables()
+    print("База очищена")
+    await create_tables()
+    print("База готова")
+    yield
+    print("Выключение")
+
+app = FastAPI(lifespan=lifespan)
+app.include_router(tasks_router)
+
+# @app.get('/')
+# def hello():
+#     print("HELLO")
+#
+#
+#
+#
+# tasks = []
+
